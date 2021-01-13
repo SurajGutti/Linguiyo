85355fd77f5f53bd1e5971514a66ec827d1ca231
sunkanmiadewumi@gmail.com <sunkanmiadewumi1@gmail.com>
2021-01-13 21:19:53 +0100

    added experience page(initial)

---
+++ frontend/src/components/culture_experience_landing/CulturalExperience.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './CulturalExperience.css'
+import EachCulture from './EachCulture'
+function CulturalExperience() {
+    return (
+        <div className="container-fluid">
+            <div className="row d-flex justify-content-center">
+                <EachCulture title="Turkish Coffee Fortune Reading" price={45} description="Turkish coffee fortune reading is an authentic tradition dating back to the Ottoman empire of the 1600s."/>
+                <EachCulture title=" Masala Chai and Pakoda the Indian Way" price={15} description="Learn the intricacies of the preparation of Indian food & some of India’s best-known dishes by taking part in a fun electronic class."/>   
+            </div>
+        </div>
+    )
+}
+
+export default CulturalExperience
---
+++ frontend/src/components/culture_experience_landing/EachCulture.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+function EachCulture({ title, description, price }) {
+    return (
+        <div className=" col-md-3 col-xs-10 m-2">
+            <div className="cultural_image_container">
+                <div className="row d-flex justify-content-center btn_cont">
+                    <div className="btn btn-success">
+                        Book Now
+                    </div>
+                </div>
+            </div>
+
+            <div className="p-2 mt-3">
+                <div className="card_bottom">
+                    <h4 className="text-center culture_title mt-3">
+                        {title}
+                    </h4>
+                    <p className="text-center price">${price}</p>
+                    <p className="text-center culture_description">{description}</p>
+                    <div className="row d-flex justify-content-center">
+                        <div className="btn btn-light">
+                            Book More
+                         </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default EachCulture
