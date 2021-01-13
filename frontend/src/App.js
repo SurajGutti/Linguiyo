a0112ced71db9289e675b9d1cf416672bc1bbdb9
Henry Le <henryle10@gmail.com>
2021-01-13 09:35:08 -0800

    added basic navbar/footer and display grid

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,20 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HelpPage from './components/help_page/help_page';
 import UserShow from './components/users/user_show';
+import HomePage from './components/home_page/home_page';
+import Navbar from './components/navbar/navbar';
+import Footer from './components/footer/footer';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <div className="App"> */}
-
-
+      <Navbar />
+      <Route exact path="/" component={HomePage} />
       <Route exact path="/help" component={HelpPage} />
       {/* use a wildcard below - /users/:id */}
       <Route exact path="/users/priya" component={UserShow} />
-
-
-      {/* </div> */}
+      <Footer />
     </BrowserRouter>
   );
 }
---
+++ frontend/src/components/navbar/navbar.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './navbar_style.css';
+import { Link } from 'react-router-dom';
+
+class Navbar extends React.Component {
+    listener = null;
+    state = {
+        nav: false
+    }
+    componentDidMount() {
+        window.addEventListener("scroll", this.handleScroll);
+    }
+    componentWillUnmount() {
+        window.removeEventListener('scroll');
+    }
+    handleScroll = () => {
+        if (window.pageYOffset > 140) {
+            if (!this.state.nav) {
+                this.setState({ nav: true });
+            }
+        } else {
+            if (this.state.nav) {
+                this.setState({ nav: false });
+            }
+        }
+
+    }
+
+    render() {
+        return (
+            <div id="navbar">
+                <Link to="/">Home</Link>
+                <Link to="/">Explore Languages</Link>
+                <Link to="/">Online Cultural Experiences</Link>
+                <Link to="/">The Hub</Link>
+                <Link to="/">Contact Us</Link>
+            </div>
+        );
+    }
+}
+
+export default Navbar;
