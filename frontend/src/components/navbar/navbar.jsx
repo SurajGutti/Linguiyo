cb8471fabd0fc3192a1eb56cad45d1a9a63d213e
Henry Le <henryle10@gmail.com>
2021-01-14 10:45:31 -0800

    made home page look like figma prototype

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -4,38 +4,17 @@ import './navbar_style.css';
 import { Link } from 'react-router-dom';
 
 class Navbar extends React.Component {
-    listener = null;
-    state = {
-        nav: false
-    }
-    componentDidMount() {
-        window.addEventListener("scroll", this.handleScroll);
-    }
-    componentWillUnmount() {
-        window.removeEventListener('scroll');
-    }
-    handleScroll = () => {
-        if (window.pageYOffset > 140) {
-            if (!this.state.nav) {
-                this.setState({ nav: true });
-            }
-        } else {
-            if (this.state.nav) {
-                this.setState({ nav: false });
-            }
-        }
-
-    }
-
     render() {
         return (
             <div id="navbar">
-                <Link to="/">Home</Link>
-                <Link to="/">Explore Languages</Link>
-                <Link to="/cultural-experience-land">Online Cultural Experiences</Link>
+                <button>Logo</button>
+                <Link className="dropdown-toggle" to="/">Languages
+                    <span className="caret"></span></Link>
+                <Link className="dropdown-toggle" to="/cultural-experience-land">Cultural Experiences
+                    <span className="caret"></span></Link>
                 <Link to="/">The Hub</Link>
-                <Link to="/">Contact Us</Link>
-            </div>
+                <button>Sign up</button>
+            </div >
         );
     }
 }
