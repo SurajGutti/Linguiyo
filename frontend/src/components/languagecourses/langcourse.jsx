ac86aa7025fbbf4b1b60fe36949c7f4cc499dc3b
Suraj Gutti <gutti.s@husky.neu.edu>
2021-01-17 21:31:47 -0500

    Structure of language page

--- frontend/src/components/languagecourses/langcourse.jsx
+++ frontend/src/components/languagecourses/langcourse.jsx
@@ -29,56 +29,54 @@ class LangCourse extends React.Component {
 
   render() {
     return (
-      <div>
-        {/* <div className="about"> */}
-        {/* <div className="teacher-about">
-                         <div className="teacher-parent">
-                             <div className="teacher-image">Teacher image</div>
-                             <div className="teacher-bio">Teacher bio</div>
-                         </div>
-                     </div> */}
-        {/* <div class="ui container"> */}
-          <div class="ui grid">
-            {/* <div class="ui row"> */}
-              <div class="five wide column">
-                <div class="ui card">
-                  <div class="image">
-                    <img src="https://semantic-ui.com/images/avatar2/large/kristy.png" />
-                  </div>
-                  <div class="content">
-                    <a class="header">Kristy</a>
-                    <div class="meta">
-                      <div class="extra content">
-                        <span class="date"><b>Occupation: </b>Teacher</span><br></br>
-                        <span class="date"><b>From: </b>Mongolia</span><br></br>
-                        <span class="date"><b>Interests: </b>Fencing</span>
-                      </div>
-                    </div>
-                    <div class="description">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Gravida dictum fusce ut placerat orci. Purus sit amet volutpat consequat mauris nunc congue nisi vitae. Lacinia at quis risus sed vulputate. Sapien pellentesque habitant morbi tristique senectus et netus et malesuada. Viverra aliquet eget sit amet tellus cras adipiscing enim eu. Velit dignissim sodales ut eu sem integer. Diam donec adipiscing tristique risus nec. Elementum integer enim neque volutpat ac tincidunt vitae semper. Purus ut faucibus pulvinar elementum integer enim neque.
-                    </div>
-                  </div>
-                  <div class="extra content">
-                    {/* <a>
-                      <i class="user icon"></i>
-                        22 Friends
-                    </a> */}
-                  </div>
-                </div>
-              </div>
-              <div class="eleven wide column">
-                <div class="ui items">
-                  <div class="item">
-                    <div class="video">Video</div>
-                    
-                    <></>
-                  </div>
-                </div>
+      <div class="main">
+        <br/>
+        <br/>
+        <h1 class="ui block header" align="center">WELCOME!</h1>
+        <div class="vid-border">
+          <iframe id="vid" width="500" height="350" src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1"></iframe>
+        </div>
+        <a href="#" class="booknowbutton">Buy Now!</a>
+        <div class="ui two column grid">
+        <div class="row">
+          <div class="column">
+            {/* <div class="card"> */}
+              <div class="image">
+                <img src="https://semantic-ui.com/images/avatar2/large/kristy.png" width="750" height="750"/>
               </div>
             {/* </div> */}
           </div>
-        {/* </div> */}
-        
+          <div class="column">
+            <div class="ui items">
+              <div class="item">
+              <div class="content" align="center">
+                <h1 class="ui header">Kristy</h1>
+                <div class="meta">
+                  <div class="extra content">
+                    <span class="date"><b>Occupation: </b>Teacher</span><br></br>
+                    <span class="date"><b>From: </b>Mongolia</span><br></br>
+                    <span class="date"><b>Interests: </b>Fencing</span>
+                  </div>
+                </div>
+                <div class="description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Gravida dictum fusce ut placerat orci. Purus sit amet volutpat consequat mauris nunc congue nisi vitae. Lacinia at quis risus sed vulputate. Sapien pellentesque habitant morbi tristique senectus et netus et malesuada. Viverra aliquet eget sit amet tellus cras adipiscing enim eu. Velit dignissim sodales ut eu sem integer. Diam donec adipiscing tristique risus nec. Elementum integer enim neque volutpat ac tincidunt vitae semper. Purus ut faucibus pulvinar elementum integer enim neque.
+                </div>
+              </div>
+              </div>
+            </div>
+          </div>
+          <div class="column"></div>
+        </div>
+          {/* <div class="ui row"> */}
+            {/* <div class="five wide column">
+              
+            </div>
+            <div class="five wide column">
+              
+            </div> */}
+          {/* </div> */}
+        </div>
+        <br/>
       </div>
     )
   }
