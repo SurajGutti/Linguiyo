4df942afca861e7bf9715a023010a192f10a8adc
sunkanmiadewumi@gmail.com <sunkanmiadewumi1@gmail.com>
2021-01-21 14:22:56 +0100

    refactored cultural experience code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,7 +21,7 @@ function App() {
       {/* use a wildcard below - /cultural-experiences/:exp-id */}
       <Route exact path="/cultural-experiences/1" component={CulturalExpShow} />
       {/* The initial cultural-experience page  */}
-      <Route exact path="/cultural-experience-land" component={CulturalExperience}/>
+      <Route exact path="/cultural-experience-main" component={CulturalExperience}/>
       {/*  language course page  1 */}
       <Route exact path="/language-courses/1" component={LangCourse}/>
       <Footer />
