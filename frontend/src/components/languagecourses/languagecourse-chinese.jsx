43813a49e640434c631b076de7ce425626ede527
Suraj Gutti <gutti.s@husky.neu.edu>
2021-02-10 10:19:33 -0500

    Changes to language course pages

--- frontend/src/components/languagecourses/languagecourse-chinese.jsx
+++ frontend/src/components/languagecourses/languagecourse-chinese.jsx
@@ -32,7 +32,6 @@ class LanguageCourseChinese extends React.Component{
                               <iframe id="vid" width="500" height="350" src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1"></iframe>
                             </div>
                             <div className="cultexp-title"/>
-                            <div className="participate-container">
                                 <div className="format-info">Course Goals</div>
                                 <div className="format-desc">
                                     By the end of the course students will be able to;<br/>
@@ -44,21 +43,10 @@ class LanguageCourseChinese extends React.Component{
                                         <div class="item">Should be able to start understanding words/phrases while watching a movie/show</div>
                                     </div>
                                 </div>
-                            </div>
-                            <div className="format-info">Linguiyo Format</div>
-                            <div className="format-desc">
-                            Linguiyo offers group and 1:1 courses offered in a virtual format. 
-                            Each course consists of 8 classes offered weekly so you can learn a new language every 2 months. 
-                            Corporate discounts are also available for groups and custom learning.
-                            </div>
                         </div>
-                            <div className="participate-title">How do I participate </div>
-                            <div className="participate-desc" style={{marginBottom: '50px'}}>
-                              Join via zoom. Download Zoom for free on a desktop or mobile device. 
-                              Once your booking is complete, you’ll receive an email with a link and details on how to join.</div>
                     </div>
                     <div className="teacher-info">
-                        <div class="ui raised card" style={{backgroundColor: '#FFD900' ,width: '70%', display: 'flex', position: 'relative', left: '15%'}}>
+                        <div class="ui raised card" style={{backgroundColor: '#FFD900' ,width: '70%', height: '70%', display: 'flex', position: 'relative', left: '15%'}}>
                             <div class="content">
                                 <div class="center aligned description">
                                     <img class="ui small circular image" style={{marginBottom: '20px', marginTop: '25px'}} src={priyahluwalia}/>
@@ -66,14 +54,14 @@ class LanguageCourseChinese extends React.Component{
                                 <div class="center aligned header" style={{marginBottom: '20px'}}>Meet Your Language Embassador</div>
                                 <div class="center aligned header" style={{marginBottom: '20px'}}>Priya Ahulwaliya</div>
                                 <div class="center aligned description">
-                                    <div class="meta" style={{marginBottom: '20px'}}>
+                                    {/* <div class="meta" style={{marginBottom: '20px'}}>
                                         <i class="linkedin in icon"></i>
                                         <span class="date">priya-ahluwalia-59101423</span><br/>
                                         <i class="envelope icon"></i>
                                         <span class="date">priyahluwalia@gmail.com</span><br/>
                                         <i class="phone icon"></i>
                                         <span class="date">415.802.5001</span><br/>
-                                    </div>
+                                    </div> */}
                                 </div>
                                 <div class="center aligned description" style={{marginBottom: '20px'}}>
                                     Serial entrepreneur, CEO @ Linguiyo, Chief Operating Officer @ PlateRate, Director of Operations @ CPIC Global, Professor, Artist
@@ -83,7 +71,7 @@ class LanguageCourseChinese extends React.Component{
                                 </div>
                             </div>
                         </div>
-                        <div class="ui raised card" style={{backgroundColor: '#FFD900' ,width: '50%', display: 'flex', position: 'relative', left: '25%'}}>
+                        <div class="ui raised card" style={{backgroundColor: '#FFD900' , width: '70%', height: '18%', display: 'flex', position: 'relative', left: '15%'}}>
                             <div class="center aligned header" style={{marginTop: '25px', marginBottom: '20px', fontWeight: "bold"}}>
                                 $120 / person
                             </div>
